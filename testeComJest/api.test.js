4cb1133152b1bd51505e994d131188fbb086da3e
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-09-01 22:08:52 -0300

    update

--- testeComJest/api.test.js
+++ testeComJest/api.test.js
@@ -1,5 +1,11 @@
-test("/get - pegar os meus users", async ()=>{
+test(":getAll - pegar os meus users", async ()=>{
     const res = await fetch("http://localhost:3000/users");
     const data = await res.json();
     expect(Array.isArray(data)).toBe(true);
-})
+});
+
+test(":getByID", async() => {
+    const res = await fetch ("http://localhost:3000/users/2")
+    const data = await res.json();
+    expect(data).toHaveProperty("id")
+});
