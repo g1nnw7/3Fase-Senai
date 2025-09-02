8314afbf5417315936c9d6dae9a0efaa706b3140
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-09-01 21:51:28 -0300

    update

---
+++ testeComJest/api.test.js
@@ -0,0 +1,5 @@
+test("/get - pegar os meus users", async ()=>{
+    const res = await fetch("http://localhost:3000/users");
+    const data = await res.json();
+    expect(Array.isArray(data)).toBe(true);
+})
