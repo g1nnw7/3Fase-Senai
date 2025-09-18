011aacd5e6a370d028fec7814ad64eaebc1b5c20
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-09-17 22:18:41 -0300

    update

--- clinic_api/prisma/seed.js
+++ clinic_api/prisma/seed.js
@@ -11,26 +11,40 @@ async function main() {
   //     },
   //   ],
   // });
+  await prisma.paciente.createMany({
+    data: [{
+      nome: "Alex",          
+      cpf: "888231421",        
+      telefone: 21421,     
+      email: "alex@gmail.com",      
+      data_nascimento:  new Date("1980-12-11"),
+      sexo: "Masculino",
+      responsavel: "" 
+    }]
+  })
 
   await prisma.exame.createMany({
     data: [{
-
+      tipo_exame: "Endoscopia",
+      resultado: "Bacteria",
+      data_exame: new Date("1980-12-11"),
+      link_arquivo: "discord.gg/link",
+      observacoes: "",
+      paciente_id: 1,
     }]
   })
 
 
   await prisma.prontuario.createMany({
     data: [{
-      
+      descricao: "Dor",
+      data: new Date("1980-12-11"),
+      medico_responsavel_id: 1,
+      paciente_id: 1
     }]
   })
 
 
-  await prisma.paciente.createMany({
-    data: [{
-
-    }]
-  })
 }
 
 main()
