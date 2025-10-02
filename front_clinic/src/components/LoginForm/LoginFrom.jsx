b35b44b28f3221eb2eabf83030104186f28b9da2
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-10-02 19:53:57 -0300

    update

--- front_clinic/src/components/LoginForm/LoginFrom.jsx
+++ front_clinic/src/components/LoginForm/LoginFrom.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 const LoginForm = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     return (
         <div className="max-w-md mx-auto mt-10 bg-white p-8 rounded-xl shadow-lg">
             <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
@@ -8,7 +12,7 @@ const LoginForm = () => {
                     <input
                         type="email"
                         id="email"
-                        value=""
+                        value={email}
                         required
                         className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 
                         focus:ring-blue-500"/>
@@ -18,7 +22,7 @@ const LoginForm = () => {
                     <input
                         type="password"
                         id="password"
-                        value=""
+                        value={password}
                         required
                         minLength={8}
                         className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2
