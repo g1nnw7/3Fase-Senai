6b61b39fc56abd76069024cf39c8e519b8354a47
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-10-02 21:29:36 -0300

    update

--- front_clinic/src/components/LoginForm/LoginFrom.jsx
+++ front_clinic/src/components/LoginForm/LoginFrom.jsx
@@ -17,6 +17,7 @@ const LoginForm = () => {
             const res = await axios.get('http://localhost:3000/users', {
                 params: { email, password }
             })
+            console.log(res)
         }
         catch {
 
