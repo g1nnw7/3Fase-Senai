ebe843580d44f617922c674807e7de4a8af1b874
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-10-09 22:06:54 -0300

    update

---
+++ front_clinic/src/components/RegisterUser/RegisterUser.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RegisterUser = () => {
+  return (
+    <div className='w-full max-w-md p-6 bg-white rouded-xl shadow-lg'>
+        <h2 className='text-2xl font-bold mb-6 text-center'>Criar Usuário</h2>
+        <form className='space-y-4'>
+            <div>
+                <label htmlFor="email" className=''></label>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default RegisterUser
